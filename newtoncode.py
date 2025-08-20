cb9279e87ed206d126dacb00d78813d221060890
Your Name <your_name@berkeley.edu>
2025-08-20 10:37:06 -0700

    Add basic implementation of Newton's method.

---
+++ newtoncode.py
@@ -0,0 +1,16 @@
+#Newton's Method
+
+#defining things 
+def f1 =(function, epsilon):
+    ((function+epsilon)-function)/epsilon
+def f2 
+def homemade(xo, function, epsilon):
+    if type(xo)==complex #is there a type called complex? should i say != integer or !numeric or !float ??
+    print("Input cannot be a complex number!") 
+    epsilon == 10**-5
+
+for i in range(0,51):
+    x[i] - (f1/f2) = x[i+1]
+    if abs(x[i+1] - x[i]) <= 0
+    return #??
+    else #keep going!
